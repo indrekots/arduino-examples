7b5d3275e6469072ebf10967ffa2ef04b20da838
Indrek Ots <ots.indrek@gmail.com>
2015-12-06 17:05:37 +0200

    implement reading from and writing to EEPROM

--- eeprom-read-write/eeprom-read-write.ino
+++ eeprom-read-write/eeprom-read-write.ino
@@ -1,9 +1,38 @@
+#include <EEPROM.h>
+String command;
+int addr = 0;
+int value = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    command = Serial.readStringUntil(' ');
+    
+    if (isValidCommand()) {
+      readParams();
 
+      if (command == "read") {
+        Serial.println(EEPROM.read(addr));
+      }
+      else {
+        EEPROM.write(addr, value);
+      }
+    } 
+  }
 }
+
+bool isValidCommand() {
+  return command == "read" || command == "write";
+}
+
+void readParams() {
+  addr = Serial.parseInt();
+  if (command == "write") {
+    value = Serial.parseInt();  
+  }
+}
+
