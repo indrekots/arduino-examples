044cdf68f9079a9e54b768e68dd19f598643ae77
Indrek Ots <ots.indrek@gmail.com>
2016-01-13 22:11:08 +0200

    wait for input from serial connection

--- optocoupler-test/optocoupler-test.ino
+++ optocoupler-test/optocoupler-test.ino
@@ -1,18 +1,30 @@
 int controlPin = 2;
 int pinStatus = LOW;
+int incomingByte = 0;
 
 void setup() {
   pinMode(2, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (pinStatus == LOW) {
-    pinStatus = HIGH; 
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    if (incomingByte == 115) {
+      digitalWrite(controlPin, HIGH);
+    }
+    else {
+      digitalWrite(controlPin, LOW);
+    }
+  
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+    delay(20);
   }
   else {
-    pinStatus = LOW;
+    digitalWrite(controlPin, LOW);
   }
-
-  digitalWrite(controlPin, pinStatus);
-  delay(500);
 }
